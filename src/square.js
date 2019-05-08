7af22a813bf3ebc732d32fee49be3fa50d382ab4
Angelos <akontogeorgis999@gmail.com>
2019-05-08 16:48:03 +0300

    nothing

--- src/square.js
+++ src/square.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';
-
- function Square ({value, onClick, isWinnerSquare}){ 
+/////
+ function Square ({value, onClick, isWinnerSquare}){
     return (
       <button
         className = {`square ${isWinnerSquare ? 'highlight-winner' : ''} `}
